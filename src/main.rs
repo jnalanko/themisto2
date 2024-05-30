16c272850fa40b68de6ec22c7f7c2be01dacb2e5
Jarno N. Alanko <alanko.jarno@gmail.com>
2024-05-30 10:13:32 +0300

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(non_snake_case, clippy::needless_range_loop)] // Using upper-case variable names from the source material
+
 use std::thread::LocalKey;
 
 use statrs::function::gamma::ln_gamma;
@@ -14,73 +16,13 @@ fn ln_beta_binom(k: usize, n: usize, alpha: f64, beta: f64) -> f64 {
     ln_binom(n,k) + ln_beta(k as f64 + alpha, (n - k) as f64 + beta) - ln_beta(alpha, beta)
 }
 
-fn pstar_normalization_factor(n: usize, alpha: f64, beta: f64) -> f64 {
-    let sum: f64 = (0..=n).map(|k| ln_pstar_unnormalized(k,n,alpha,beta)).map(|x| x.exp()).sum();
-    return 1.0 / sum;
-}
-
 // p^* in Eq. 2.7 in Tommi's thesis
 fn ln_pstar_unnormalized(k: usize, n: usize, alpha: f64, beta: f64) -> f64 {
     ln_binom(n,k) + ln_beta(k as f64 + alpha, (n - k) as f64 + beta) - ln_beta(n as f64 + alpha, beta)
 }
 
-fn ln_pstar(k: usize, n: usize, alpha: f64, beta: f64) -> f64 {
-    ln_pstar_unnormalized(k, n, alpha, beta) + pstar_normalization_factor(n, alpha, beta).ln()
-}
-
-fn ln_pstar_normalized_thesis_formula(k: usize, n: usize, alpha: f64, beta: f64) -> f64 {
-    let mut ans = ln_pstar_unnormalized(k, n, alpha, beta);
-    for j in 1..=n {
-        let n = n as f64;
-        let k = k as f64;
-        ans += (alpha + n + k - j as f64).ln() - (alpha + beta + 2.0*n - j as f64).ln();
-    } 
-    ans
-}
-
-// The input is a weighted compatibility matrix where cell (i,j) has the number of
-// compatible genomes in cluster j for read i.
-// have size 1, the hit counts are 0 or 1.
-// The latent variables (I_n in the paper) are color ids, one for each read.
-fn evalulate_model(hit_counts: &Vec<Vec<usize>>, latent_variables: &Vec<usize>, mixing_proportions: &Vec<f64>) -> f64 {
-    // Assuming a flat prior on the mixing proportions theta
-    // p(r_{n, k} | I_n = k) * p(I_n = k | theta)
-    // Is it the case that p(I_n = k | theta) is just theta_n?
-
-    // The likelihood term p(r_{n, k} | I_n = k)...
-    // Ordinarily, without clustered references, this would be... I guess...:
-    // 0.01 if r_{n,k} = 0, else 0.99
-    //
-    // But now, with a clustered reference, we have a refinement: Let M_k be the number of genomes in cluster k.
-    // Then, we have 0.01 if r_{n,k} as above, but otherwise we have 0.99 * f(r_{n,k}, M_k). The thesis says that
-    // a beta-binomial distribution for f(...) would be a reasonable first approximation. It has two hyperparameters
-    // alpha and beta. But, then f would not be increasing as a function of hit count, so they define a modified
-    // version of the beta-binomial distribution to fix this. There is a lot of math involved in finding the right
-    // normalization constant.
-
-    let term1 = |_,k| mixing_proportions[k];
-    let term2 = |n: usize, k: usize| {
-        if hit_counts[n][k] == 0 {
-            0.01
-        } else if hit_counts[n][k] == 1 {
-            0.99
-        } else {
-            0.99 // todo: here the beta-binomial thing
-        }
-    };
-
-    let N = latent_variables.len(); // Number of reads
-    let k = hit_counts.first().unwrap().len(); // Number of clusters
-    let mut ans = 1_f64;
-    for n in 0..N {
-        for k in 0..k {
-            ans *= term1(n, k)* term2(n,k);
-        }
-    }
-    ans
-}
-
-// Likelihood_matrix[i][j] is the likelihood of read i vs cluster j
+// Returns p(theta | R)
+// Likelihood_matrix[i][j] is the likelihood of read j vs cluster i
 // Flat prior on theta
 fn theta_posterior(likelihood_matrix: &Vec<Vec<f64>>, theta: &Vec<f64>) -> f64 {
     let k = likelihood_matrix.len();
@@ -93,9 +35,10 @@ fn theta_posterior(likelihood_matrix: &Vec<Vec<f64>>, theta: &Vec<f64>) -> f64 {
     ans
 }
 
-// Likelihood_matrix[i][j] is the likelihood of read i vs cluster j
+// Returns p(theta | R)
+// Likelihood_matrix[i][j] is the likelihood of read j vs cluster i
 // Flat prior on theta
-fn theta_posterior_with_indicator_variables(likelihood_matrix: &Vec<Vec<f64>>, theta: &Vec<f64>) -> f64 {
+fn theta_posterior_exponential_formula(likelihood_matrix: &Vec<Vec<f64>>, theta: &Vec<f64>) -> f64 {
     let k = likelihood_matrix.len();
     let n = likelihood_matrix.first().unwrap().len();
     let mut ans = 0_f64;
@@ -118,12 +61,45 @@ fn theta_posterior_with_indicator_variables(likelihood_matrix: &Vec<Vec<f64>>, t
     ans.ln()
 }
 
+// Takes in R[0..K)[0..N)
+// R[k][n] = number of pseudoalignments from read n to color cluster k
+// cluster_sizes[k] = number of colors in cluster k
+// Returns a matrix L[0..K)[0..N) such that L[k][n] is the likelihood that read r_n
+// is from cluster k.
+fn build_likelihood_matrix(R: &Vec<Vec<usize>>, cluster_sizes: &Vec<usize>) -> Vec<Vec<f64>> {
+    let K = R.len();
+    let N = R.first().unwrap().len();
+    let mut L = vec![vec![0_f64; N]; K];
+
+    let pi: f64 = 0.65; // From thesis
+
+    for n in 0..N {
+        for k in 0..K {
+            L[k][n] = if R[k][n] == 0 {
+                0.01
+            } else if R[k][n] == 1 && cluster_sizes[k] == 1 {
+                0.99
+            } else {
+                let phi = 1.0 - pi + 0.01 / cluster_sizes[k] as f64;
+                let alpha = pi / phi;
+                let beta = (1.0 - pi) / phi;
+                dbg!(pi, phi, alpha, beta, k, n);
+                0.99 * ln_pstar_unnormalized(R[k][n], cluster_sizes[k], alpha, beta).exp()
+            }
+        }
+    }
+
+    L
+}
+
 fn main() {
 
-    let likelihood_matrix = vec![vec![0.1, 0.1], vec![0.6, 0.7], vec![0.5, 0.5]];
-    let theta = vec![0.0, 0.5, 0.5];
+    let cluster_sizes = vec![3,1,4]; // Max number of hits to each cluster
+    let R = vec![vec![3,2,1,0], vec![1,0,1,0], vec![0,2,4,4]]; // 4 reads, 3 clusters
+    let likelihood_matrix = build_likelihood_matrix(&R, &cluster_sizes);
+    let theta = vec![0.3, 0.3, 0.4];
     eprintln!("{}", theta_posterior(&likelihood_matrix, &theta));
-    eprintln!("{}", theta_posterior_with_indicator_variables(&likelihood_matrix, &theta));
+    //eprintln!("{}", 
 
     /*
     let n = 10;
