2d2f9086d25bbeb9889384f2021d58166ede7ca0
Jarno N. Alanko <alanko.jarno@gmail.com>
2025-11-15 19:59:36 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -207,7 +207,7 @@ fn print_color_sets<CSS: ColorSetStorage>(index: &CompactColexColoring<CSS>, que
     let stdout = std::io::stdout();
     let mut out = BufWriter::new(stdout);
 
-    log::info!("Printing color sets for query sequences in {}", query_path.display());
+    log::info!("Retrieving color sets for query sequences in {}", query_path.display());
 
     while let Some(rec) = reader.read_next().unwrap() {
         let sets = index.lookup_kmer_color_sets(rec.seq);
