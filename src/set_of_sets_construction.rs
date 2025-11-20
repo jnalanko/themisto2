4b1b7f7485a08487aeb745dd19ea27bd5c45bfc7
Jarno N. Alanko <alanko.jarno@gmail.com>
2025-11-20 16:43:21 +0200

    Comment

--- src/set_of_sets_construction.rs
+++ src/set_of_sets_construction.rs
@@ -61,7 +61,8 @@ impl<T : Iterator<Item = SetElement>> ColorSetStream for MyTransposedColorSetStr
 
 /// Takes a generator of SetElement structs with set_id in 0..max_n_sets and element in 0..max_n_elements.
 /// The element generators must generate the elements in increasing order of element: first all set ids
-/// with element 0, then all set ids with element 1, and so on.
+/// with element 0, then all set ids with element 1, and so on. There must not be duplicate elements
+/// in the same set.
 fn construct<CSS: ColorSetStorage>(
     element_generator: impl Iterator<Item = SetElement>, 
     element_generator_again: impl Iterator<Item = SetElement>, 
