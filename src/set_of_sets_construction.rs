2d71fa854cb193d4a602980d80ce1ac8e2801efe
Jarno N. Alanko <alanko.jarno@gmail.com>
2025-11-21 11:46:17 +0200

    Remove debug print

--- src/set_of_sets_construction.rs
+++ src/set_of_sets_construction.rs
@@ -101,7 +101,7 @@ pub fn construct_from_generators_that_do_not_give_duplicates<CSS: ColorSetStorag
         set_sizes.resize_with(n_sets, || AtomicU64::new(0));
 
         element_generator.for_each(|new| { 
-            dbg!(&new);
+            //dbg!(&new);
             // TODO: this doesn't really parallize this way. Instead of this, I need iterators
             // for pieces of the input, where each initializes its own matching statistics algorithm.
             let (fp1, fp2) = element_fingerprints[new.color];
