80c8ee6334e2f81dc64634f4883704885392c301
Jarno N. Alanko <alanko.jarno@gmail.com>
2025-12-03 13:31:36 +0200

    Flush unitigs out

--- src/colex_colored_kmers.rs
+++ src/colex_colored_kmers.rs
@@ -805,6 +805,7 @@ impl<CSS: ColorSetStorage> CompactColexKmers<CSS> {
                     }
                     colex += 1;
                 }
+                unitigs_out.flush().unwrap();
                 log::info!("Found {} cyclic unitigs", unitig_id - n_acyclic);
                 unitig_id
             });
