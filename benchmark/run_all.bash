850e72cbffbb71b1e4f6a7e23863f89cf1dae003
Alanko Jarno N <analanko@turso03.cs.helsinki.fi>
2025-12-03 02:04:22 +0200

    --from-unitigs

--- benchmark/run_all.bash
+++ benchmark/run_all.bash
@@ -6,11 +6,11 @@ set -xueo pipefail
 
 /usr/bin/time -v themisto2 build -s sbwt/unitigs.sbwt -i fof/seqs.txt -o index/seqs.thm2 --temp-dir temp -k 31 -d 30 -t 32 --index-type sparse-dense 2>&1 | tee logs/seqs.log
 
-/usr/bin/time -v themisto2 build -s sbwt/unitigs.sbwt -i fof/unitigs.txt -o index/unitigs.thm2 --temp-dir temp -k 31 -d 30 -t 32 --index-type sparse-dense 2>&1 | tee logs/unitigs.log
+/usr/bin/time -v themisto2 build -s sbwt/unitigs.sbwt -i fof/unitigs.txt -o index/unitigs.thm2 --temp-dir temp -k 31 -d 30 -t 32 --index-type sparse-dense --from-unitigs 2>&1 | tee logs/unitigs.log
 
-/usr/bin/time -v themisto2 build -s sbwt/unitigs-half1.sbwt -i fof/unitigs-half1.txt -o index/half1.thm2 --temp-dir temp -k 31 -d 30 -t 32 --index-type sparse-dense 2>&1 | tee logs/half1.log
+/usr/bin/time -v themisto2 build -s sbwt/unitigs-half1.sbwt -i fof/unitigs-half1.txt -o index/half1.thm2 --temp-dir temp -k 31 -d 30 -t 32 --index-type sparse-dense --from-unitigs 2>&1 | tee logs/half1.log
 
-/usr/bin/time -v themisto2 build -s sbwt/unitigs-half2.sbwt -i fof/unitigs-half2.txt -o index/half2.thm2 --temp-dir temp -k 31 -d 30 -t 32 --index-type sparse-dense 2>&1 | tee logs/half2.log
+/usr/bin/time -v themisto2 build -s sbwt/unitigs-half2.sbwt -i fof/unitigs-half2.txt -o index/half2.thm2 --temp-dir temp -k 31 -d 30 -t 32 --index-type sparse-dense --from-unitigs 2>&1 | tee logs/half2.log
 
 /usr/bin/time -v themisto2 merge --index-file-list fof/merge.txt -o index/merge.thm2 --temp-dir temp -d 30 -t 32 2>&1 | tee logs/merge.log
 
