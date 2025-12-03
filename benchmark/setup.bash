d8f202f57ce6a83e9abf68351d1962c34b6cbb22
Alanko Jarno N <analanko@kale-816.local.cs.helsinki.fi>
2025-12-03 03:52:57 +0200

    Fix setup bash script

--- benchmark/setup.bash
+++ benchmark/setup.bash
@@ -14,9 +14,9 @@ ls seqs/ | xargs --verbose -P 16 -I {} ggcat build -p -s 1 -m 2 --temp-dir temp 
 SEQ_COUNT=$(find seqs -maxdepth 1 -type f | wc -l)
 HALF_COUNT=$(( SEQ_COUNT / 2 ))
 
-find seqs -type f | grep ".fna$" | sort > fof/seqs.txt
+find seqs -type f | grep "\.fna" | sort > fof/seqs.txt # Might be gzipped
 
-find unitigs/ -type f | grep ".fna$" | sort > fof/unitigs.txt
+find unitigs/ -type f | grep ".fna$" | sort > fof/unitigs.txt # Are not gzipped because they come from ggcat
 
 find unitigs/ -type f | grep ".fna$" | sort | head -n $HALF_COUNT > fof/unitigs-half1.txt
 find unitigs/ -type f | grep ".fna$" | sort | tail -n $HALF_COUNT > fof/unitigs-half2.txt
